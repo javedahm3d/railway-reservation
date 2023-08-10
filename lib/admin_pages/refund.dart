fcc5ae01906b529901caad0c77078f606d3976ac
Javed Chaudhary <jachoudhary@oneshield.com>
2023-08-10 09:43:30 +0530

    10th aug 9am

--- lib/admin_pages/refund.dart
+++ lib/admin_pages/refund.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../pages/homepage.dart';
@@ -15,6 +16,7 @@ class _RefundsPageState extends State<RefundsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(221, 209, 225, 238),
       appBar: AppBar(
         toolbarHeight: 60,
         backgroundColor: Colors.deepPurple,
@@ -37,53 +39,77 @@ class _RefundsPageState extends State<RefundsPage> {
           ],
         ),
       ),
-      body: SingleChildScrollView(
-          child: StreamBuilder(
-              stream:
-                  FirebaseFirestore.instance.collection('refund').snapshots(),
-              builder: (context,
-                  AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('refund').snapshots(),
+          builder: (context,
+              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
 
-                if (snapshot.data!.docs.length == 0) {
-                  return Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Center(child: Text('no active refunds')),
-                  );
-                }
-                return ListView.builder(
-                  itemCount: snapshot.data!.docs.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-                      child: Card(
-                        child: Container(
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20)),
-                          child: Row(
-                            children: [
-                              Text(snapshot.data!.docs[index]
-                                  .data()['transaction id']),
-                              Spacer(),
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: ElevatedButton(
-                                    onPressed: () {}, child: Text('active')),
-                              )
-                            ],
+            if (snapshot.data!.docs.length == 0) {
+              return Padding(
+                padding: const EdgeInsets.all(20),
+                child: Center(child: Text('no active refunds')),
+              );
+            }
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                  child: Card(
+                    child: Container(
+                      width: double.infinity,
+                      height: 80,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding:
+                                const EdgeInsets.all(8.0).copyWith(left: 20),
+                            child: Text(
+                              snapshot.data!.docs[index]
+                                  .data()['transaction id']
+                                  .toString(),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w600, fontSize: 20),
+                            ),
                           ),
-                        ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.all(8.0).copyWith(left: 15),
+                            child: ElevatedButton(
+                                onPressed: () {
+                                  final data = ClipboardData(
+                                      text: snapshot.data!.docs[index]
+                                          .data()['transaction id']);
+                                  Clipboard.setData(data);
+                                },
+                                child: Text(
+                                  'copy',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                )),
+                          ),
+                          Spacer(),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ElevatedButton(
+                                onPressed: () {}, child: Text('active')),
+                          )
+                        ],
                       ),
-                    );
-                  },
+                    ),
+                  ),
                 );
-              })),
+              },
+            );
+          }),
       drawer: Drawer(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
